97758ed9bc96e9263c3a2c57852be63cd3ff010c
Chicaiza Eduardo <echicaiza6997@uta.edu.ec>
2026-05-07 00:44:35 -0500

    creacion de clase prueba tipo calculo de promedio basico

---
+++ ControlVersiones/src/main/java/com/mycompany/controlversiones/PruebaEduardoChicaiza.java
@@ -0,0 +1,38 @@
+
+package com.mycompany.controlversiones;
+
+public class PruebaEduardoChicaiza {
+   private double nota1; 
+   private double nota2;
+
+    public PruebaEduardoChicaiza(double nota1, double nota2) {
+        this.nota1 = nota1;
+        this.nota2 = nota2;
+    }
+
+    public double getNota1() {
+        return nota1;
+    }
+
+    public void setNota1(double nota1) {
+        this.nota1 = nota1;
+    }
+
+    public double getNota2() {
+        return nota2;
+    }
+
+    public void setNota2(double nota2) {
+        this.nota2 = nota2;
+    }
+   
+    public double calcularPromedio(){
+        double promedio = (this.nota1 + this.nota2)/2;
+        return promedio;
+    }
+    
+    public static void main(String[] args) {
+        PruebaEduardoChicaiza obj = new PruebaEduardoChicaiza(9.5, 7.5);
+        System.out.println("El promedio es: " + obj.calcularPromedio());
+    }
+}
