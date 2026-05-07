ea27983a566e57c2326cc2da02fc0b27aa170599
CristianDDGA <cango7999@uta.edu.ec>
2026-05-07 08:03:34 -0500

    Creacion de PruebaCristian

---
+++ ControlVersiones/src/main/java/com/mycompany/controlversiones/PruebaCristian.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.controlversiones;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class PruebaCristian {
+    public String cedulaMia;
+    public String nombreMio;
+    public String apellidoMio;
+    public String direccionMia;
+
+    public PruebaCristian(String cedulaMia, String nombreMio, String apellidoMio, String direccionMia) {
+        this.cedulaMia = cedulaMia;
+        this.nombreMio = nombreMio;
+        this.apellidoMio = apellidoMio;
+        this.direccionMia = direccionMia;
+    }
+    
+    
+    
+}
