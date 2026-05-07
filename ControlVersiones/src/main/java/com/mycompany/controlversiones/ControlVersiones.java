db13a1ef9bc6c88bc49ca5fa3bea53e92e90021f
CristianDDGA <cango7999@uta.edu.ec>
2026-05-06 22:12:38 -0500

    Prueba para rollback

--- ControlVersiones/src/main/java/com/mycompany/controlversiones/ControlVersiones.java
+++ ControlVersiones/src/main/java/com/mycompany/controlversiones/ControlVersiones.java
@@ -14,6 +14,6 @@ public class ControlVersiones {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Cristian Ango"+ new Date()); 
+        
    }
 }
