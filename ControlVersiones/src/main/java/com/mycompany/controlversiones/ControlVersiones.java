b6b07865699a4766259c61852ea3ee9e0d0a0124
CristianDDGA <cango7999@uta.edu.ec>
2026-05-06 22:15:29 -0500

    Revert "Prueba 1 Control de versiones por: Cristian Ango"
    This reverts commit 38907a2ea12fee2d35da4ea54a138dab34f2f9f6.

--- ControlVersiones/src/main/java/com/mycompany/controlversiones/ControlVersiones.java
+++ ControlVersiones/src/main/java/com/mycompany/controlversiones/ControlVersiones.java
@@ -4,8 +4,6 @@
 
 package com.mycompany.controlversiones;
 
-import java.util.Date;
-
 /**
  *
  * @author Lenovo
@@ -14,6 +12,5 @@ public class ControlVersiones {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Cristian Ango"+ new Date()); 
-   }
+    }
 }
