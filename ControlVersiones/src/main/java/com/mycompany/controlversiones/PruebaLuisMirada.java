2b64cd7f4b84177f118aed396ec7b47a52e2ebba
LuisMiranda0 <lmiranda2016@uta.edu.ec>
2026-05-07 00:15:11 -0500

    feat: Añade PruebaLuisMiranda class con constructor, getters and setters

---
+++ ControlVersiones/src/main/java/com/mycompany/controlversiones/PruebaLuisMirada.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.controlversiones;
+
+/**
+ *
+ * @author Windows 11
+ */
+public class PruebaLuisMirada {
+    private int id;
+    private String nombre;
+    private double precio;
+    private boolean disponible;
+
+    public PruebaLuisMirada(int id, String nombre, double precio, boolean disponible) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.disponible = disponible;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+    
+    
+}
